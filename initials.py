b765d50548311edc86376ea41bd07cbf14cbbb99
Jerrykuda2 <2425400879@live.gctu.edu.gh>
2026-05-29 21:42:03 -0700

    Add files via upload

--- initials.py
+++ initials.py
@@ -1,7 +1,30 @@
+# File: initials.py
+# Description: Print out my initials.
+# Assignment Number: 1
+#
+# Name: Danso Jeremiah Kumi
+# STUDENT ID:  2425400879
+# Email: 2425400879@.gctu.edu.gh
+# Grader: Augustus
+#
+# On my honor, Augustus Buckman, this programming assignment is my own work
+# and I have not provided this code to any other student.
 
 def main():
-  pass #code goes here
-
-
-
-main()
+    # Prints initials JKD in small letters and as large stylized block letters
+    
+ print()
+ print("...JKD")
+ print()
+ print("...     JJJJJJ ...  ...KK      KK  ...  ...DDDDDDD     ...")
+ print("...       JJ   ...  ...KK    KK    ...  ...DD    DDD   ...")
+ print("...       JJ   ...  ...KK   KK     ...  ...DD      DD  ...")
+ print("...       JJ   ...  ...KK  KK      ...  ...DD      DD  ...")
+ print("...       JJ   ...**...KKKK        ...**...DD      DD  ...**")
+ print("...       JJ   ...**...KKKK        ...**...DD      DD  ...**")
+ print("...JJ     JJ   ...  ...KK  KK      ...  ...DD      DD  ...")
+ print("...JJ     JJ   ...  ...KK   KK     ...  ...DD      DD  ...")
+ print("... JJ   JJ    ...  ...KK    KK    ...  ...DD    DDD   ...")
+ print("...  JJJJJ     ...  ...KK      KK  ...  ...DDDDDDD     ...")
+ print()
+main
